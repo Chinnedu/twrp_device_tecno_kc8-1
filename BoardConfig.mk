7fa50df59d7977fa489b78c7ae294e044382529a
infinixs5lite <kelvinchinedu97@gmail.com>
2023-03-04 23:24:16 +0100

    Update BoardConfig.mk

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -5,7 +5,7 @@
 # SPDX-License-Identifier: Apache-2.0
 #
 
-DEVICE_PATH := device/tecno/KC8
+DEVICE_PATH := device/tecno/kc8
 
 # For building with minimal manifest
 ALLOW_MISSING_DEPENDENCIES := true
